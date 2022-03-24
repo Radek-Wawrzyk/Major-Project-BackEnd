7b8e0b2127ee2930ab28e9fee2075d4f37b05ebf
Radziu <radekwawrzyk@gmail.com>
2022-03-24 17:41:34 +0000

    Setup - DB setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+    }),
+  ],
+})
 export class AppModule {}
