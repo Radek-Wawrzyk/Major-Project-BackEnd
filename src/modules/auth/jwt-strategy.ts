21d78ecdc15f9cb2df39b9b9887c0c6581075d01
Radziu <radekwawrzyk@gmail.com>
2022-03-28 21:32:08 +0100

    Auth - Set missing JWT secret for jwt-strategy.ts file

--- src/modules/auth/jwt-strategy.ts
+++ src/modules/auth/jwt-strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'JWT Secret',
+      secretOrKey: `${process.env.JWT_SECRET_KEY}`,
     });
   }
 
